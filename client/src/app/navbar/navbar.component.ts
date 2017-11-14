2b8f4059f9cb6e0725a8e1fb67f370a3bfd70c66
Vikthor Vergara <vikthorferreira@gmail.com>
2017-11-14 15:28:41 -0200

    client: componentes basicos

---
+++ client/src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { MaterializeDirective } from "angular2-materialize";
+import { Component } from "@angular/core"
+import { Location } from '@angular/common';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+    // routeNames = ["Buttons", "Carousel", "Chips", "Collapsible", "Dialogs", "Dropdown", "Forms", "Tabs", "DatePicker", "Parallax", "ModelBindings"];
+}
