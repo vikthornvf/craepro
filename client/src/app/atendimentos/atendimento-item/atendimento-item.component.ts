c07cc840c178bc3d4a1ed746c2ed040cfbdf5986
Vikthor <vikthorferreira@gmail.com>
2017-11-19 05:01:17 -0200

    client: oitimizacao de codigo com ng-content

--- client/src/app/atendimentos/atendimento-item/atendimento-item.component.ts
+++ client/src/app/atendimentos/atendimento-item/atendimento-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Atendimento } from '../atendimento.model';
 import { Aluno } from '../../alunos/aluno.model';
 
@@ -11,17 +11,8 @@ import { Aluno } from '../../alunos/aluno.model';
 	templateUrl: './atendimento-item.component.html',
 	styleUrls: ['./atendimento-item.component.css']
 })
-export class AtendimentoItemComponent implements OnInit {
+export class AtendimentoItemComponent {
 
-	@Input() fonte: any;
 	@Input() atendimento: Atendimento;
 	@Input() selectedId: string;
-	isAluno: boolean;
-
-	ngOnInit(): void {
-		if (!this.fonte) {
-			return;
-		}
-		this.isAluno = (this.fonte.escola);
-	}
 }
