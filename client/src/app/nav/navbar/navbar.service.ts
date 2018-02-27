18bd806f9b2a119c304f2380ee8d9fb9319015bf
Vikthor <vikthorferreira@gmail.com>
2018-02-27 15:44:52 -0300

    added new toolbar component

--- client/src/app/nav/navbar/navbar.service.ts
+++ client/src/app/nav/navbar/navbar.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { deprecate } from 'util';
 
 @Injectable()
 export class NavbarService {
 
+	state = {
+		NAVBAR: 0,
+		SEARCHBAR: 1,
+		TOOLBAR: 2
+	};
+
 	private keywordSource  = new BehaviorSubject<string>('');
 	keyword = this.keywordSource.asObservable();
 
+	private navbarStateSource = new BehaviorSubject<number>(this.state.NAVBAR);
+	navbarState = this.navbarStateSource.asObservable();
+
+	/*
+	 * @deprecated
+	 */
 	private navbarSearchSource = new BehaviorSubject<boolean>(false);
 	navbarSearch = this.navbarSearchSource.asObservable();
+	changeNavbarSearch(navbarSearch: boolean) {
+		this.navbarSearchSource.next(navbarSearch);
+	}
 
 	changeKeyword(keyword: string) {
 		this.keywordSource.next(keyword);
 	}
 
-	changeNavbarSearch(navbarSearch: boolean) {
-		this.navbarSearchSource.next(navbarSearch);
+	changeState(state: number) {
+		this.navbarStateSource.next(state);
 	}
 }
