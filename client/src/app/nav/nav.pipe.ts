bc76ec64066a3f1bccfa8ed0b849dceb16de3bc8
Vikthor <vikthorferreira@gmail.com>
2018-02-28 23:20:54 -0300

    better search engine

--- client/src/app/nav/nav.pipe.ts
+++ client/src/app/nav/nav.pipe.ts
@@ -4,8 +4,41 @@ import { Pipe, PipeTransform } from '@angular/core';
 	name: 'navFilter'
 })
 export class NavFilter implements PipeTransform {
-	transform(list, keyword, prop) {
-		keyword = keyword.toLowerCase();
-		return list.filter(entity => entity[prop].toLowerCase().includes(keyword));
+
+	accentMap = {
+		'à': 'a',
+		'á': 'a',
+		'â': 'a',
+		'ã': 'a',
+		'ç': 'c',
+		'é': 'e',
+		'è': 'e',
+		'ê': 'e',
+		'í': 'i',
+		'ì': 'i',
+		'î': 'i',
+		'ñ': 'n',
+		'ó': 'o',
+		'ò': 'o',
+		'ô': 'o',
+		'õ': 'o',
+		'ú': 'u',
+		'ù': 'u',
+		'û': 'u'
+	};
+
+	chars = /[aceinou]/g;
+
+	transform(list, keyword, property) {
+		keyword = this.standardizeChars(keyword.toLowerCase());
+		return list.filter(entity => this.standardizeChars(entity[property].toLowerCase()).includes(keyword));
+	}
+
+	standardizeChars(input: string): string {
+		let output = '';
+		for (let i = 0; i < input.length; i++) {
+			output += this.accentMap[input.charAt(i)] || input.charAt(i);
+		}
+		return output;
 	}
 }
