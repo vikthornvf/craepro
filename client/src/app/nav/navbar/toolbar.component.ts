59228148574b1ac4d0533206e53fcfc05162f56e
Vikthor <vikthorferreira@gmail.com>
2018-03-02 18:11:40 -0300

    toolbar component is responsible for the navigatio to edit pages

--- client/src/app/nav/navbar/toolbar.component.ts
+++ client/src/app/nav/navbar/toolbar.component.ts
@@ -11,16 +11,23 @@ export class ToolbarComponent implements OnInit, OnDestroy {
 
 	hasAtt = false;
 	hasAttObservable: Subscription;
+
+	link: string[];
+	linkObservable: Subscription;
+
 	tool = this.navService.tools;
 
 	constructor(private navService: NavbarService) {}
 
 	ngOnInit(): void {
+		const path = this.navService.path();
 		this.hasAttObservable = this.navService.hasAtt.subscribe(hasAtt => this.hasAtt = hasAtt);
+		this.linkObservable = this.navService.link.subscribe(link => this.link = [path].concat(link));
 	}
 
 	ngOnDestroy(): void {
 		this.hasAttObservable.unsubscribe();
+		this.linkObservable.unsubscribe();
 	}
 
 	onEmitTool(tool: number) {
