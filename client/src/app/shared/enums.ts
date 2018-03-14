aa4e97edf85a323edbba358bad819e9635c934a7
Vikthor <vikthorferreira@gmail.com>
2018-03-14 19:44:10 -0300

    helper enums

---
+++ client/src/app/shared/enums.ts
@@ -0,0 +1,43 @@
+export class Enums {
+
+	static readonly Atts = [
+		{ value: 'A', name: 'AEE' },
+		{ value: 'P', name: 'Psicológico' },
+		{ value: 'F', name: 'Fonoaudiológico' }
+	];
+
+	static readonly Series = [
+		{ value: 'E3', name: '3º ano' },
+		{ value: 'E2', name: '2º ano' },
+		{ value: 'E1', name: '1º ano' },
+		{ value: 'F9', name: '9ª série' },
+		{ value: 'F8', name: '8ª série' },
+		{ value: 'F7', name: '7ª série' },
+		{ value: 'F6', name: '6ª série' },
+		{ value: 'F5', name: '5ª série' },
+		{ value: 'F4', name: '4ª série' },
+		{ value: 'F3', name: '3ª série' },
+		{ value: 'F2', name: '2ª série' },
+		{ value: 'F1', name: '1ª série' },
+		{ value: 'PB', name: 'Pré' },
+		{ value: 'PA', name: 'Pré A' },
+		{ value: 'B2', name: 'Berçário II' },
+		{ value: 'B1', name: 'Berçário ' },
+		{ value: 'M3', name: 'Maternal III' },
+		{ value: 'M2', name: 'Maternal II' },
+		{ value: 'M1', name: 'Maternal' }
+	];
+
+	static readonly Status = [
+		{ value: 'A', name: 'Ativo' },
+		{ value: 'P', name: 'Parcialmente Ativo' },
+		{ value: 'E', name: 'Em espera' },
+		{ value: 'D', name: 'Desligado' }
+	]
+
+	static readonly Turnos = [
+		{ value: 'M', name: 'Manhã' },
+		{ value: 'T', name: 'Tarde' },
+		{ value: 'N', name: 'Noite' }
+	];
+}
