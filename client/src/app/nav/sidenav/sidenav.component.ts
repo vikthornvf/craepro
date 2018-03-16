024800d13ea0b63a133ee8dbd94d8663db186e6e
Vikthor <vikthorferreira@gmail.com>
2018-03-16 19:37:46 -0300

    on mobile: closes the side nav when select a link

--- client/src/app/nav/sidenav/sidenav.component.ts
+++ client/src/app/nav/sidenav/sidenav.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $;
+
 @Component({
 	selector: 'app-sidenav',
 	templateUrl: './sidenav.component.html',
 	styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent {
+
 	routes = [
 		{ name: 'alunos', label: 'Alunos' },
 		{ name: 'professores', label: 'Professores' },
 		{ name: 'escolas', label: 'Escolas' },
 		{ name: 'usuarios', label: 'Usuários' }
 	];
+
+	onNavigate($event: Event): void {
+		$event.preventDefault();
+		$('.button-collapse').sideNav('hide');
+	}
 }
