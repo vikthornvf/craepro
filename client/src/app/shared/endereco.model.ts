3ee77a758b7a57e92f9db5b26853bac0ad051cd7
Vikthor <vikthorferreira@gmail.com>
2018-05-08 15:03:36 -0300

    add model endereco

---
+++ client/src/app/shared/endereco.model.ts
@@ -0,0 +1,10 @@
+export class Endereco {
+
+	constructor(
+		public cidade?: string,
+		public tipo?: string,
+		public rua?: string,
+		public numero?: number,
+		public bairro?: string,
+		public complemento?: string) {}
+}
