e3c20c63b956671fbb8de1072012be65e613e30c
Vikthor <vikthorferreira@gmail.com>
2018-05-08 15:02:24 -0300

    usage of callback to confirm

--- client/src/app/delete-confirmation/delete-confirmation.component.ts
+++ client/src/app/delete-confirmation/delete-confirmation.component.ts
@@ -1,6 +1,7 @@
 import { MaterializeDirective, MaterializeAction } from 'angular2-materialize';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+// TODO transform into a service like toasts
 @Component({
 	selector: 'app-delete-confirmation',
 	templateUrl: './delete-confirmation.component.html'
@@ -9,6 +10,7 @@ export class DeleteConfirmationComponent {
 
 	@Input() text;
 	@Output() response: EventEmitter<boolean> = new EventEmitter<false>();
+	callback: Function;
 
 	label: string;
 	labeled = false;
@@ -17,18 +19,22 @@ export class DeleteConfirmationComponent {
 	modalParams = [{ complete: () => this.onModalClose() }];
 	res = false;
 
-	open() {
-		this.labeled = false;
-		this.modalActions.emit({ action: 'modal', params: ['open'] });
-	}
-
-	openWithLabel(label: string) {
-		this.labeled = true;
-		this.label = label;
+	open(label?: string, callback?: Function) {
+		if (label) {
+			this.labeled = true;
+			this.label = label;
+		}
+		if (callback) {
+			this.callback = callback;
+		}
 		this.modalActions.emit({ action: 'modal', params: ['open'] });
 	}
 
 	onModalClose() {
+		this.labeled = false;
 		this.response.emit(this.res);
+		if (this.callback && this.callback instanceof Function) {
+			this.callback(this.res);
+		}
 	}
 }
