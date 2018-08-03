cc66ee1761da1fefdaccdc3af66f28b491e62822
Vikthor <vikthorferreira@gmail.com>
2018-08-03 12:50:22 -0300

    client: better nav component control

--- client/src/app/usuarios/usuario-login-pane/usuario-login-pane.component.ts
+++ client/src/app/usuarios/usuario-login-pane/usuario-login-pane.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavService } from '../../nav/nav.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 declare var Materialize: any;
 
@@ -7,12 +9,21 @@ declare var Materialize: any;
 	templateUrl: './usuario-login-pane.component.html',
 	styleUrls: ['./usuario-login-pane.component.css']
 })
-export class UsuarioLoginPaneComponent implements OnInit {
+export class UsuarioLoginPaneComponent implements OnInit, OnDestroy {
 
 	login = true;
 
+	constructor(private navService: NavService) {}
+
 	ngOnInit() {
+		this.navService.onHideSidebar(true);
+		this.navService.onHideTopbar(true);
 		this.updateTextFields();
+	}
+
+	ngOnDestroy() {
+		this.navService.onHideSidebar(false);
+		this.navService.onHideTopbar(false);
 	}
 
 	toggleLogin() {
