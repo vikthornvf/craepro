6819b61ff81352ec38c49362fab4b716d4c174ea
Vikthor <vikthorferreira@gmail.com>
2018-08-29 19:37:48 -0300

    client: angular import convention

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,38 +1,37 @@
-import { MaterializeSharedModule } from './shared/materialize-shared.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { NavModule } from './nav/nav.module';
-import { DialogsModule } from './dialogs/dialogs.module';
-import { NotFoundModule } from './not-found/not-found.module';
-import { HomeModule } from './home/home.module';
-// import { DashboardModule } from './dashboard/dashboard.module';
-import { AlunoModule } from './alunos/aluno.module';
-import { ProfissionalModule } from './profissionais/profissional.module';
-import { EscolaModule } from './escolas/escola.module';
-import { UsuarioModule } from './usuarios/usuario.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterializeSharedModule } from './shared/materialize-shared.module';
+import { AlunoModule } from './alunos/aluno.module';
+// import { DashboardModule } from './dashboard/dashboard.module';
+import { DialogsModule } from './dialogs/dialogs.module';
+import { EscolaModule } from './escolas/escola.module';
+import { HomeModule } from './home/home.module';
+import { NavModule } from './nav/nav.module';
+import { NotFoundModule } from './not-found/not-found.module';
+import { ProfissionalModule } from './profissionais/profissional.module';
+import { UsuarioModule } from './usuarios/usuario.module';
 import { AuthService } from './auth.service';
 import { AuthGuardService } from './auth-guard.service';
-import { LoginGuardService } from './login-guard.service';
 import { StorageService } from './storage.service';
-
+import { LoginGuardService } from './login-guard.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
-		MaterializeSharedModule,
-		BrowserModule,
 		HttpClientModule,
-		NavModule,
-		DialogsModule,
-		NotFoundModule,
-		HomeModule,
-		// DashboardModule,
+		BrowserModule,
+		MaterializeSharedModule,
 		AlunoModule,
-		ProfissionalModule,
+		// DashboardModule,
+		DialogsModule,
 		EscolaModule,
+		HomeModule,
+		NavModule,
+		NotFoundModule,
+		ProfissionalModule,
 		UsuarioModule
 	],
 	providers: [
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { UsuarioLoginPaneComponent } from './usuarios/usuario-login-pane/usuario-login-pane.component';
-import { HomeComponent } from './home/home.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-import { AlunoListComponent } from './alunos/aluno-list/aluno-list.component';
 import { AlunoComponent } from './alunos/aluno/aluno.component';
-import { ProfissionalListComponent } from './profissionais/profissional-list/profissional-list.component';
-import { ProfissionalComponent } from './profissionais/profissional/profissional.component';
-import { EscolaListComponent } from './escolas/escola-list/escola-list.component';
+import { AlunoListComponent } from './alunos/aluno-list/aluno-list.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 import { EscolaComponent } from './escolas/escola/escola.component';
-import { UsuarioListComponent } from './usuarios/usuario-list/usuario-list.component';
-import { UsuarioComponent } from './usuarios/usuario/usuario.component';
-import { UsuarioProfileComponent } from './usuarios/usuario-profile/usuario-profile.component';
-import { UsuarioPasswordComponent } from './usuarios/usuario-password/usuario-password.component';
+import { EscolaListComponent } from './escolas/escola-list/escola-list.component';
+import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfissionalComponent } from './profissionais/profissional/profissional.component';
+import { ProfissionalListComponent } from './profissionais/profissional-list/profissional-list.component';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
+import { UsuarioListComponent } from './usuarios/usuario-list/usuario-list.component';
+import { UsuarioLoginPaneComponent } from './usuarios/usuario-login-pane/usuario-login-pane.component';
+import { UsuarioPasswordComponent } from './usuarios/usuario-password/usuario-password.component';
+import { UsuarioProfileComponent } from './usuarios/usuario-profile/usuario-profile.component';
 
-import { LoginGuardService } from './login-guard.service';
 import { AuthGuardService } from './auth-guard.service';
+import { LoginGuardService } from './login-guard.service';
 
 
-const appRoutes: Routes = [
+const routes: Routes = [
 	{ path: '', component: HomeComponent, pathMatch: 'full'  },
 	{ path: 'login', component: UsuarioLoginPaneComponent, canActivate: [LoginGuardService] },
 	{ path: 'login/:path', component: UsuarioLoginPaneComponent, canActivate: [LoginGuardService] },
@@ -43,7 +43,10 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(appRoutes)],
+	imports: [RouterModule.forRoot(routes, {
+		scrollPositionRestoration: 'enabled',
+		anchorScrolling: 'enabled'
+	})],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
