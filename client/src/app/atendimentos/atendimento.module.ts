8c8d87cdff1de3d9e253a74e51fcb7f6cc2078fd
Vikthor <vikthorferreira@gmail.com>
2018-08-29 11:35:21 -0300

    client: removed unnecessary export

--- client/src/app/atendimentos/atendimento.module.ts
+++ client/src/app/atendimentos/atendimento.module.ts
@@ -3,8 +3,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MaterializeSharedModule } from '../shared/materialize-shared.module';
-import { DirectivesModule } from '../shared/directives.module';
 import { PipesModule } from '../pipes/pipes.module';
+import { DirectivesModule } from '../shared/directives.module';
 import { AtendimentoComponent } from './atendimento/atendimento.component';
 import { AtendimentoListComponent } from './atendimento-list/atendimento-list.component';
 import { AtendimentoModalComponent } from './atendimento-modal/atendimento-modal.component';
@@ -17,7 +17,6 @@ import { AtendimentoService } from './atendimento.service';
 		AtendimentoComponent
 	],
 	exports: [
-		AtendimentoModalComponent,
 		AtendimentoListComponent,
 		AtendimentoComponent
 	],
