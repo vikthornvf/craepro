13ad339e65e5541f032a8edc9dd0558b9b901cce
Vikthor <vikthorferreira@gmail.com>
2018-08-29 17:55:18 -0300

    client: better homebar component

--- client/src/app/nav/navbar/homebar.component.ts
+++ client/src/app/nav/navbar/homebar.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $;
 
 @Component({
 	selector: 'app-homebar',
 	templateUrl: './homebar.component.html',
 	styleUrls: ['./homebar.component.css']
 })
-export class HomebarComponent {}
+export class HomebarComponent implements OnInit {
+
+	ngOnInit(): void {
+		this.initDropdown();
+	}
+
+	initDropdown() {
+		$(document).ready(function() {
+			$('.dropdown-button').dropdown();
+		});
+	}
+}
