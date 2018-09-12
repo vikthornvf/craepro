70397ea40705e3dbd6bf7edc53ad1a8897c1fb28
Vikthor <vikthorferreira@gmail.com>
2018-09-12 18:45:22 -0300

    fixed component imports

--- public/src/app/nav/nav.module.ts
+++ public/src/app/nav/nav.module.ts
@@ -5,11 +5,11 @@ import { NgModule } from '@angular/core';
 import { MaterializeSharedModule } from '../shared/materialize-shared.module';
 import { AppRoutingModule } from '../app.routes';
 import { NavComponent } from './nav.component';
-import { HomebarComponent } from './navbar/homebar.component';
+import { HomebarComponent } from './navbar/homebar/homebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SearchbarComponent } from './navbar/searchbar.component';
+import { SearchbarComponent } from './navbar/searchbar/searchbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { ToolbarComponent } from './navbar/toolbar.component';
+import { ToolbarComponent } from './navbar/toolbar/toolbar.component';
 import { NavFilter } from './nav.pipe';
 import { NavService } from './nav.service';
 
